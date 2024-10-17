8bb197476b8913e39a6723d8cfbab94967b7bf71
liangxq777 <85600533+liangxq777@users.noreply.github.com>
2024-10-17 18:13:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,12 @@ mkdir -p /node/logs
 
 cd /node
 
-# sudo apt update 
-# sudo apt install supervisor
-#sudo apt install git openjdk-8-jdk -y
-#wget https://raw.githubusercontent.com/liangxq777/node/refs/heads/main/main_net_config.conf
-#wget https://github.com/tronprotocol/java-tron/releases/download/GreatVoyage-v4.7.6/FullNode.jar
-#wget https://github.com/tronprotocol/java-tron/releases/download/GreatVoyage-v4.7.6/Toolkit.jar
-
+sudo apt update 
+sudo apt install supervisor
+sudo apt install git openjdk-8-jdk -y
+wget https://raw.githubusercontent.com/liangxq777/node/refs/heads/main/main_net_config.conf
+wget https://github.com/tronprotocol/java-tron/releases/download/GreatVoyage-v4.7.6/FullNode.jar
+wget https://github.com/tronprotocol/java-tron/releases/download/GreatVoyage-v4.7.6/Toolkit.jar
 wget https://raw.githubusercontent.com/liangxq777/node/refs/heads/main/tron_daemon.conf
 
 mv tron_daemon.conf /etc/supervisor/conf.d/
